84f72cd03e6ae79b33dc903932ed63a337f82aa0
zhanghang <2426705212@qq.com>
2021-01-06 10:50:25 +0800

    提交修改代码

---
+++ src/main/java/com/example/test_mysql/model/TestModel.java
@@ -0,0 +1,14 @@
+package com.example.test_mysql.model;
+
+import lombok.Data;
+
+@Data
+public class TestModel {
+
+    private Integer Id;
+
+    private String modelType;
+
+    private String modelName;
+
+}
